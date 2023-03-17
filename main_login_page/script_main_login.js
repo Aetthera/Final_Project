b6c47b52471380fcd544050ea6dcb5b639d5f114
Alena <alenabelovadmvn@gmail.com>
2023-03-17 14:25:22 -0400

    login validation
    login validation works for name and password. Login btn is verifying the name to be filled and password to be filled and 6 characters minimum. As well loin btn now leads to second page with a next form.

--- main_login_page/script_main_login.js
+++ main_login_page/script_main_login.js
@@ -2,5 +2,23 @@ const wrapper = document.querySelector ('.wrapper');
 const btn = document.querySelector ('.btnlogin');
 const iconClose = document.querySelector ('.icon-close');
 
+//btn login and close events 
 btn.addEventListener('click', ()=> {wrapper.classList.add('active');});
 iconClose.addEventListener('click', ()=> {wrapper.classList.remove ('active');});
+
+
+//user validation
+function validateForm() {
+    let name = document.forms["login-form"]["username"].value;
+    if (name == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+
+    let pass = document.forms["login-form"]["pwd"].value;
+    if (pass.length < 5) {
+      alert("Password must be filled out and contain minimum of 6 characters");
+      return false;
+    }
+  }  
+
