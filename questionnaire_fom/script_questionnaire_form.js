22187cfe9889ca76bb30d23d69591f924f9e1bdf
Alena <alenabelovadmvn@gmail.com>
2023-03-24 17:02:51 -0400

    article del and about +
    Deleted  folder article plus aded content into about page (css html and visible content as text)

--- questionnaire_fom/script_questionnaire_form.js
+++
@@ -1,8 +0,0 @@
-const wrapper = document.querySelector ('.wrapper');
-const btn = document.querySelector ('.btnStart');
-const iconClose = document.querySelector ('.icon-close');
-
-//btn login and close events 
-btn.addEventListener('click', ()=> {wrapper.classList.add('active');});
-iconClose.addEventListener('click', ()=> {wrapper.classList.remove ('active');});
-
